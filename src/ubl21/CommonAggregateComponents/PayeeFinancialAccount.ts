c0017ccd80ff0ecd9c6f329e1694714b01c68a1d
Catalin Comanescu <catalin.comanescu@ciel.ro>
2023-12-21 22:26:00 +0200

    Fix payee financial account

--- src/ubl21/CommonAggregateComponents/PayeeFinancialAccount.ts
+++ src/ubl21/CommonAggregateComponents/PayeeFinancialAccount.ts
@@ -1,11 +1,12 @@
 import GenericAggregateComponent, { IGenericKeyValue, ParamsMapValues } from './GenericAggregateComponent';
-import { UdtIdentifier } from '../types/UnqualifiedDataTypes';
+import { UdtIdentifier, UdtText } from '../types/UnqualifiedDataTypes';
 import { FinancialInstitutionBranch } from './FinancialInstitutionBranch';
 
 const ParamsMap: IGenericKeyValue<ParamsMapValues> = {
   id: { order: 1, attributeName: 'cbc:ID', min: 0, max: 1, classRef: UdtIdentifier },
-  financialInstitutioBranch: {
-    order: 2,
+  name: { order: 2, attributeName: 'cbc:Name', min: 0, max: 1, classRef: UdtText },
+  financialInstitutionBranch: {
+  order: 3,
     attributeName: 'cac:FinancialInstitutionBranch',
     min: 0,
     max: 1,
@@ -15,7 +16,7 @@ const ParamsMap: IGenericKeyValue<ParamsMapValues> = {
 
 type AllowedParams = {
   id: string | UdtIdentifier;
-  financialInstitutioBranch?: FinancialInstitutionBranch;
+  financialInstitutionBranch?: FinancialInstitutionBranch;
 };
 
 class PayeeFinancialAccount extends GenericAggregateComponent {
